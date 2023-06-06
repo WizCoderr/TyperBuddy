143c2da987a6921c8cb8e6fd2c7f776dc6bd9c45
nitesh-dev <niteshdev547@gmail.com>
2023-06-06 13:30:48 +0530

    keyboard working

--- front/components/Keyboard.vue
+++ front/components/Keyboard.vue
@@ -1,214 +1,246 @@
 <script setup lang='ts'>
 
+let allKeyElements: NodeListOf<Element>
 
+onMounted(function () {
+    window.onkeydown = onKeyDown
+    window.onkeyup = onKeyUp
+    allKeyElements = document.querySelectorAll("#keyboard .row>div")
+})
+
+
+function onKeyDown(event: KeyboardEvent) {
+    console.log(event.code)
+    highlightKey(event.code, "active")
+}
+
+function onKeyUp(event: KeyboardEvent) {
+    //console.log(event.code)
+    highlightKey(event.code)
+}
+
+function highlightKey(key: string, className: string = '') {
+
+    allKeyElements.forEach(keyElement => {
+        if (keyElement.getAttribute("data-key") == key) {
+
+            if(className == ''){
+                keyElement.classList.remove("active")
+            }else{
+                keyElement.classList.add("active")
+            }
+            return
+        }
+    });
+}
 
 
 </script>
 <template>
     <div class="keyboard-container">
-        <div class="keyboard">
+        <div class="keyboard" id="keyboard">
             <div class="row row-14">
-                <div class="square multi">
+                <div data-key="Backquote" class="square multi">
                     <span>~</span>
-                    <span>`</span>
+                    <span>&#96;</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit1" class="square multi">
                     <span>!</span>
                     <span>1</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit2" class="square multi">
                     <span>@</span>
                     <span>2</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit3" class="square multi">
                     <span>#</span>
                     <span>3</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit4" class="square multi">
                     <span>$</span>
                     <span>4</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit5" class="square multi">
                     <span>%</span>
                     <span>5</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit6" class="square multi">
                     <span>^</span>
                     <span>6</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit7" class="square multi">
                     <span>&</span>
                     <span>7</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit8" class="square multi">
                     <span>*</span>
                     <span>8</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit9" class="square multi">
                     <span>&#40;</span>
                     <span>9</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Digit0" class="square multi">
                     <span>&#41;</span>
                     <span>0</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Minus" class="square multi">
                     <span>&#8212;</span>
                     <span>-</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Equal" class="square multi">
                     <span>+</span>
                     <span>=</span>
                 </div>
-                <div class="wide-2 single">
+                <div data-key="Backspace" class="wide-2 single">
                     <span>Backspace</span>
                 </div>
             </div>
             <div class="row row-14">
-                <div class="wide-1-5 single">
+                <div data-key="" class="wide-1-5 single">
                     <span>Tab</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyQ" class="square single">
                     <span>Q</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyW" class="square single">
                     <span>W</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyE" class="square single">
                     <span>E</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyR" class="square single">
                     <span>R</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyT" class="square single">
                     <span>T</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyY" class="square single">
                     <span>Y</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyU" class="square single">
                     <span>U</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyI" class="square single">
                     <span>I</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyO" class="square single">
                     <span>O</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyP" class="square single">
                     <span>P</span>
                 </div>
-                <div class="square multi">
+                <div data-key="BracketLeft" class="square multi">
                     <span>{</span>
                     <span>[</span>
                 </div>
-                <div class="square multi">
+                <div data-key="BracketRight" class="square multi">
                     <span>}</span>
                     <span>]</span>
                 </div>
-                <div class="wide-1-5 multi">
+                <div data-key="Backslash" class="wide-1-5 multi">
                     <span>|</span>
                     <span>\</span>
                 </div>
             </div>
             <div class="row row-13">
-                <div class="wide-2 single">
+                <div data-key="CapsLock" class="wide-2 single">
                     <span>Caps Lock</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyA" class="square single">
                     <span>A</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyS" class="square single">
                     <span>S</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyD" class="square single">
                     <span>D</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyF" class="square single">
                     <span>F</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyG" class="square single">
                     <span>G</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyH" class="square single">
                     <span>H</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyJ" class="square single">
                     <span>J</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyK" class="square single">
                     <span>K</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyL" class="square single">
                     <span>L</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Semicolon" class="square multi">
                     <span>:</span>
                     <span>;</span>
                 </div>
-                <div class="square multi">
+                <div data-key="quote" class="square multi">
                     <span>"</span>
                     <span>'</span>
                 </div>
-                <div class="wide-2-1 single">
+                <div data-key="Enter" class="wide-2-1 single">
                     <span>Enter</span>
                 </div>
             </div>
             <div class="row row-12">
-                <div class="wide-2-3 single">
+                <div data-key="ShiftLeft" class="wide-2-3 single">
                     <span>Shift</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyZ" class="square single">
                     <span>Z</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyX" class="square single">
                     <span>X</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyC" class="square single">
                     <span>C</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyV" class="square single">
                     <span>V</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyB" class="square single">
                     <span>B</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyN" class="square single">
                     <span>N</span>
                 </div>
-                <div class="square single">
+                <div data-key="KeyM" class="square single">
                     <span>M</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Comma" class="square multi">
                     <span>&#60;</span>
                     <span>,</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Period" class="square multi">
                     <span>&#62;</span>
                     <span>.</span>
                 </div>
-                <div class="square multi">
+                <div data-key="Slash" class="square multi">
                     <span>?</span>
                     <span>/</span>
                 </div>
-                <div class="wide-2-5 single">
+                <div data-key="ShiftRight" class="wide-2-5 single">
                     <span>Shift</span>
                 </div>
             </div>
             <div class="row row-5">
-                <div class="wide-2 single">
+                <div data-key="ControlLeft" class="wide-2 single">
                     <span>Ctrl</span>
                 </div>
-                <div class="wide-2 single">
+                <div data-key="" class="wide-2 single">
                     <span>Alt</span>
                 </div>
-                <div class="wide-6 single">
+                <div data-key="Space" class="wide-6 single">
                     <span>Space</span>
                 </div>
-                <div class="wide-2 single">
+                <div data-key="" class="wide-2 single">
                     <span>Alt</span>
                 </div>
-                <div class="wide-2 single">
+                <div data-key="ControlRight" class="wide-2 single">
                     <span>Ctrl</span>
                 </div>
             </div>
@@ -264,10 +296,11 @@
 .keyboard .row>div {
     background-color: white;
     border-radius: var(--keyboard-key-radius);
+    transition: all 50ms;
 }
 
 .keyboard .row .active {
-    background-color: var(--color-primary);
+    background-color: var(--color-primary-variant);
     color: white;
 }
 
