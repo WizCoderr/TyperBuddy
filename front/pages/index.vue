72be6c504304483a58030713e5d229725c5a53ab
nitesh-dev <niteshdev547@gmail.com>
2023-06-11 21:49:49 +0530

    some ui  bugs fixed

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -40,9 +40,9 @@ function updateTypingReport(reportData: TypingReport) {
     // calculating maxValue and minValue
     reportData.keysReport.forEach(data => {
         const change = data.correctCount - data.errorCount
-        if(change > keysMaxValue){
+        if (change > keysMaxValue) {
             keysMaxValue = change
-        }else if(change < keysMinValue){
+        } else if (change < keysMinValue) {
             keysMinValue = change
         }
     });
@@ -82,13 +82,26 @@ function updateTypingReport(reportData: TypingReport) {
         <Sidebar />
         <section class="main">
             <div class="status-bar">
-                <h3>Speed: {{ currentTypingReport.averageSpeed }} ( {{ typingSpeedPerformance }})</h3>
-                <h3>Error: {{ currentTypingReport.errorCount }} ( {{ typingAccuracyPerformance }} )</h3>
+                <h3>Speed: {{ currentTypingReport.averageSpeed }} ( <span
+                        :class="{ 'success': typingSpeedPerformance[0] == '+' }">{{ typingSpeedPerformance }}</span> )</h3>
+                <h3>Error: {{ currentTypingReport.errorCount }} ( <span
+                        :class="{ 'success': typingAccuracyPerformance[0] == '+' }">{{ typingAccuracyPerformance }}</span> )
+                </h3>
+                <div>
+                    <button>
+                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M12.012 2.25c.734.008 1.465.093 2.182.253a.75.75 0 0 1 .582.649l.17 1.527a1.384 1.384 0 0 0 1.927 1.116l1.401-.615a.75.75 0 0 1 .85.174 9.792 9.792 0 0 1 2.204 3.792.75.75 0 0 1-.271.825l-1.242.916a1.381 1.381 0 0 0 0 2.226l1.243.915a.75.75 0 0 1 .272.826 9.797 9.797 0 0 1-2.204 3.792.75.75 0 0 1-.848.175l-1.407-.617a1.38 1.38 0 0 0-1.926 1.114l-.169 1.526a.75.75 0 0 1-.572.647 9.518 9.518 0 0 1-4.406 0 .75.75 0 0 1-.572-.647l-.168-1.524a1.382 1.382 0 0 0-1.926-1.11l-1.406.616a.75.75 0 0 1-.849-.175 9.798 9.798 0 0 1-2.204-3.796.75.75 0 0 1 .272-.826l1.243-.916a1.38 1.38 0 0 0 0-2.226l-1.243-.914a.75.75 0 0 1-.271-.826 9.793 9.793 0 0 1 2.204-3.792.75.75 0 0 1 .85-.174l1.4.615a1.387 1.387 0 0 0 1.93-1.118l.17-1.526a.75.75 0 0 1 .583-.65c.717-.159 1.45-.243 2.201-.252Zm0 1.5a9.135 9.135 0 0 0-1.354.117l-.109.977A2.886 2.886 0 0 1 6.525 7.17l-.898-.394a8.293 8.293 0 0 0-1.348 2.317l.798.587a2.881 2.881 0 0 1 0 4.643l-.799.588c.32.842.776 1.626 1.348 2.322l.905-.397a2.882 2.882 0 0 1 4.017 2.318l.11.984c.889.15 1.798.15 2.687 0l.11-.984a2.881 2.881 0 0 1 4.018-2.322l.905.396a8.296 8.296 0 0 0 1.347-2.318l-.798-.588a2.881 2.881 0 0 1 0-4.643l.796-.587a8.293 8.293 0 0 0-1.348-2.317l-.896.393a2.884 2.884 0 0 1-4.023-2.324l-.11-.976a8.988 8.988 0 0 0-1.333-.117ZM12 8.25a3.75 3.75 0 1 1 0 7.5 3.75 3.75 0 0 1 0-7.5Zm0 1.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+                        </svg>
+                        <span>Settings</span>
+                    </button>
+                </div>
             </div>
             <div class="key-status">
                 <h3>All Keys:</h3>
                 <div class="keys">
-                    <div v-for="item in currentTypingReport.keysReport" :style="'background-color:' + getKeyColor(keysMinValue, keysMaxValue, item.correctCount - item.errorCount)">
+                    <div v-for="item in currentTypingReport.keysReport"
+                        :style="'background-color:' + getKeyColor(keysMinValue, keysMaxValue, item.correctCount - item.errorCount)">
                         <span>{{ item.key }}</span>
                     </div>
                 </div>
@@ -125,11 +138,49 @@ main {
     display: flex;
     flex-direction: row;
     gap: 4rem;
+    align-items: center;
+    margin-bottom: 1rem;
+}
+
+.main .status-bar div{
+    display: flex;
+    margin-left: auto;
+}
+
+.main .status-bar button{
+    display: flex;
+    align-items: center;
+    background-color: var(--color-secondary);
+    border: none;
+    color: var(--color-on-secondary);
+    font-size: var(--average-font);
+    padding: 0.6rem 1rem;
+    border-radius: var(--border-radius);
+    fill: var(--color-on-secondary);
+    gap: 1rem;
+}
+
+.main .status-bar button:hover{
+    background-color: var(--color-secondary-variant);
+}
+
+.main .status-bar button svg{
+    fill: inherit;
 }
 
 .main .status-bar h3 {
     white-space: nowrap;
+    margin: 0;
 }
+
+.main .status-bar h3 span {
+    color: var(--color-error);
+}
+
+.main .status-bar h3 span.success {
+    color: var(--color-success);
+}
+
 
 .main .key-status {
     display: flex;
@@ -240,5 +291,4 @@ main {
     main {
         grid-template-columns: 220px auto;
     }
-}
-</style>
+}</style>
