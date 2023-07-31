fdd754dc9c36c898f634dcb3f2b321ff1e0dda4d
nitesh-dev <niteshdev547@gmail.com>
2023-07-31 12:48:12 +0530

    profile view added

--- front/components/Sidebar.vue
+++ front/components/Sidebar.vue
@@ -27,6 +27,20 @@ function closeSignInDialog() {
                 <img src="../public/favicon.ico" alt="TyperBuddy">
                 <h2>TyperBuddy</h2>
             </div>
+
+            <hr>
+            <div class="profile">
+                <img src="../public/extra/no_image.png">
+                <h4>Nitesh Kr</h4>
+            </div>
+
+            <button @click="openSignInDialog()" class="button primary">
+                <span>SignIn</span>
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M21.25 4.5a.75.75 0 0 1 .743.648L22 5.25v13.5a.75.75 0 0 1-1.493.102l-.007-.102V5.25a.75.75 0 0 1 .75-.75Zm-9.04 1.887.083-.094a1 1 0 0 1 1.32-.083l.094.083 4.997 4.998a1 1 0 0 1 .083 1.32l-.083.093-4.996 5.004a1 1 0 0 1-1.499-1.32l.083-.094L15.581 13H3a1 1 0 0 1-.993-.883L2 12a1 1 0 0 1 .883-.993L3 11h12.584l-3.291-3.293a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z" />
+                </svg>
+            </button>
             <hr>
             <ul>
                 <li :class="{ 'active': activeTabIndex == 0 }">
@@ -109,15 +123,6 @@ function closeSignInDialog() {
                 </li>
 
             </ul>
-
-            <hr>
-            <button @click="openSignInDialog()" class="button primary">
-                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M21.25 4.5a.75.75 0 0 1 .743.648L22 5.25v13.5a.75.75 0 0 1-1.493.102l-.007-.102V5.25a.75.75 0 0 1 .75-.75Zm-9.04 1.887.083-.094a1 1 0 0 1 1.32-.083l.094.083 4.997 4.998a1 1 0 0 1 .083 1.32l-.083.093-4.996 5.004a1 1 0 0 1-1.499-1.32l.083-.094L15.581 13H3a1 1 0 0 1-.993-.883L2 12a1 1 0 0 1 .883-.993L3 11h12.584l-3.291-3.293a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z" />
-                </svg>
-                <span>SignIn</span>
-            </button>
         </nav>
     </div>
 
@@ -144,7 +149,36 @@ function closeSignInDialog() {
 .sidebar button {
     width: 90%;
     margin: auto;
+    justify-content: space-between;
 
+}
+
+
+/* ------------ profile ---------- */
+
+.sidebar .profile{
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    margin:  0 0.6rem;
+    padding: 0 0.4rem;
+    background-color: rgb(238, 238, 238);
+    border-radius: var(--border-radius);
+
+    /* temp */
+    display: none;
+}
+
+.sidebar .profile:hover{
+    background-color: rgb(219, 219, 219);
+}
+
+.sidebar .profile img{
+    width: 42px;
+    height: 42px;
+    object-fit: cover;
+    border-radius: 50%;
+    border: 1px solid rgb(202, 202, 202);
 }
 
 
