edc09938f287af97151ba36cb1d47b26be1b92d6
nitesh-dev <niteshdev547@gmail.com>
2024-02-08 21:34:39 +0530

    added robot.txt and remove some from from indexing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    "@pinia/nuxt",
-    "@nuxtjs/sitemap"
+  modules: ["@pinia/nuxt", "@nuxtjs/sitemap", "@nuxtjs/robots"],
+  css: [
+    "@/assets/css/global.css",
+    "@/assets/css/skeleton.css",
+    "vue-toast-notification/dist/theme-sugar.css",
   ],
-  css: ["@/assets/css/global.css", "@/assets/css/skeleton.css", "vue-toast-notification/dist/theme-sugar.css"],
   pinia: {
     // autoImports: [
     //   // automatically imports `defineStore`
@@ -15,5 +16,8 @@ export default defineNuxtConfig({
   imports: {
     autoImport: false,
   },
-
+  sitemap: {
+    xsl: false,
+    exclude: ["/auth-verification/**", "/wallet", "/tournaments/**"],
+  },
 });
